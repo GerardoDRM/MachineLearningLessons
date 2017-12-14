be0da568bb5de46e2d1b3f605f7808c5b1d814da
gerardodrm <gerardo.delarosama@udlap.mx>
2017-12-14 14:50:10 -0600

    Trying KNN

--- Udacity/choose_your_own/your_algorithm.py
+++ Udacity/choose_your_own/your_algorithm.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -28,16 +30,13 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+ac = accuracy_score(pred, labels_test)
+print(ac)
 ### visualization code (prettyPicture) to show you the decision boundary
-
-
-
-
-
-
-
-
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:

363495f0dd5b0263ce0564f36f93ce948b3a5f22
gerardodrm <gerardo.delarosama@udlap.mx>
2017-12-17 21:03:42 -0600

    Outliers on enron data

--- Udacity/outliers/enron_outliers.py
+++ Udacity/outliers/enron_outliers.py
@@ -6,14 +6,28 @@ import matplotlib.pyplot
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+from outlier_cleaner import outlierCleaner
+from sklearn.linear_model import LinearRegression
+
+'''
+  We found that there are more outliers on enron data,
+  this outliers own to people that can be suspicius on the case
+  so this is an example where outliers can show us some info that
+  represents points that we need to pay attention
+ '''
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+# Remove outlier TOTAL key
+data_dict.pop("TOTAL",0)
 data = featureFormat(data_dict, features)
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-### your code below
-
-
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()

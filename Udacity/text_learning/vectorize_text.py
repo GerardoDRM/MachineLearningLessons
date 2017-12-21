c084b0e0e83de64cbbde5658cf4b1e99c63a71f8
gerardodrm <gerardo.delarosama@udlap.mx>
2017-12-20 18:47:16 -0600

    Trying TIFD

--- Udacity/text_learning/vectorize_text.py
+++ Udacity/text_learning/vectorize_text.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 """
     Starter code to process the emails from Sara and Chris to extract
     the features and get the documents ready for classification.
@@ -41,24 +41,33 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        # temp_counter += 1
+        # if temp_counter < 200:
+        path = os.path.join('..', path[:-1])
+        print path
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        ### use parseOutText to extract the text from the opened email
+        text = parseOutText(email)
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        signatures = ["sara", "shackleton", "chris", "germani"]
+        for word in signatures:
+            text = text.replace(word, "")
+        ### append the text to word_data
+        word_data.append(text)
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        from_data.append(0 if name == "sara" else 1)
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        email.close()
 
-            ### append the text to word_data
+print("DATA",word_data[152])
+# Remove stopwords
+vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer.fit_transform(word_data)
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
-
+print(len(vectorizer.get_feature_names()))
+print(vectorizer.get_feature_names()[34597])
 print "emails processed"
 from_sara.close()
 from_chris.close()
@@ -71,5 +80,3 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
